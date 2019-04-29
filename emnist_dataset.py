06b62258865ecf9ed9a272ede3f833183d331d6e
Julian L Euler <julinksky@googlemail.com>
2019-04-29 15:25:18 +0200

    v0.1 of ex01. Needs to be tested somehow

--- emnist_dataset.py
+++ emnist_dataset.py
@@ -1,77 +1,58 @@
 from emnist import list_datasets, extract_training_samples, extract_test_samples
 import numpy as np
-import random
-import array
 
-# get train_images and labels
+def prepare_samples(samples, label_min, label_max):
+    """Prepare samples for usage.
+    Filter out unwanted samples (e.g. idx_min=10, idx_max=35 for uppercase letters).
+    Shuffle the label-image-pairs and cut them all to the same length for a balanced dataset.
+    """
+    images, labels = samples
 
-images_train, labels_train = extract_training_samples('byclass')
+    filtered_indices = np.where((labels >= label_min) & (labels <= label_max))
+    labels = np.take(labels, filtered_indices) # filter out unwanted samples in labels
+    images = np.take(images, filtered_indices, axis=0) # filter out unwanted samples in images
+    labels = labels - label_min # smallest index is now 0
 
-index_uppercase = np.where((labels_train >= 10) & (labels_train <= 35))  # filter out needed uppercase letters
-labels_train = np.take(labels_train, index_uppercase)
-labels_train = labels_train - 10;  # now indices are from 0 to 25
+    labels = np.squeeze(labels)
+    images = np.squeeze(images) # discard unnecessary extra dimension from take
 
-label_values = np.bincount(labels_train[0, :])  # count occurences of all labels
-smallest_label = np.argmin(label_values)  # index of least occurent label
-smallest_label_value = np.amin(label_values)  # count of least occurent label
+    label_occs = np.bincount(labels) # count occurences of all labels
+    smallest_label = np.argmin(label_occs) # index of least occuring label
+    smallest_label_cnt = np.amin(label_occs) # count of least occuring label
 
-random.shuffle(labels_train[0, :])  # randomly shuffled
-labels_train = np.squeeze(labels_train)  # squeezing used to discard unnecessary dimensions
+    rdm_state = np.random.get_state() # save the random state of numpy
+    np.random.shuffle(labels) # randomly shuffle the labels
+    np.random.set_state(rdm_state) # load the previous random state
+    np.random.shuffle(images) #randomly shuffle the images in the same way
 
-# pick smallest_label_value amount
-train_index = array.array('i')  # integer array init
-for x in range(0, 26):
-    ind = np.squeeze(np.where(labels_train == x))
-    ind = ind[:smallest_label_value]
-    train_index = np.append(train_index, ind)
+    index_array = np.array([], dtype=int) #create an empty array
+    for i in range(label_max-(label_min-1)): # loop through labels
+        indices = np.squeeze(np.where(labels == i)) # get the indices of the current label
+        indices = indices[:smallest_label_cnt] # cut the size down to the smallest
+        index_array = np.append(index_array, indices) # append the indices
 
-bilder_train = np.squeeze(images_train[train_index, :, :])
+    labels = np.squeeze(labels[index_array])
+    images = np.squeeze(images[index_array, :, :]) # balance the labels and images
 
+    return images, labels
 
-
-
-# get test_images and labels (test data is roughly 1/6 of train data,
-# checked by comparing smallest_label_value and smallest_label_value_test)
-
-images_test, labels_test = extract_test_samples('byclass')
-
-index_uppercase = np.where((labels_test >= 10) & (labels_test <= 35))  # filter out needed uppercase letters
-labels_test = np.take(labels_test, index_uppercase)
-labels_test = labels_test - 10;  # now indices are from 0 to 25
-
-label_values = np.bincount(labels_test[0, :])  # count occurences of all labels
-smallest_label = np.argmin(label_values)  # index of least occurent label
-smallest_label_value_test = np.amin(label_values)  # count of least occurent label
-
-random.shuffle(labels_test[0, :])  # randomly shuffled
-labels_test = np.squeeze(labels_test)  # squeezing used to discard unnecessary dimensions
-
-# pick smallest_label_value amount
-test_index = array.array('i')  # balanced integer array init
-for x in range(0, 26):
-    ind = np.squeeze(np.where(labels_test == x))
-    ind = ind[:smallest_label_value_test]
-    test_index = np.append(test_index, ind)
-
-bilder_test = np.squeeze(images_test[test_index, :, :])
-
+train_samples = prepare_samples(extract_training_samples('byclass'), 10, 36)
+test_samples = prepare_samples(extract_test_samples('byclass'), 10, 36)
 
 
 class structured_array():
-    def __init__(self, name, picture_data,labels):
+    def __init__(self, name, sample_data):
         self.name = name
-        self.pictures = picture_data
-        self.labels = labels
+        self.images, self.labels = sample_data
 
 # emnist_dataset is final dataset
 emnist_dataset = []
 # for training: emnist_dataset[0].name,emnist_dataset[0].pictures,emnist_dataset[0].labels
-emnist_dataset.append(structured_array("Training", bilder_train,labels_train))
+emnist_dataset.append(structured_array("Training", train_samples))
 # for testing: emnist_dataset[1].name,emnist_dataset[1].pictures,emnist_dataset[1].labels
-emnist_dataset.append(structured_array("Testing", bilder_test,labels_test))
+emnist_dataset.append(structured_array("Testing", test_samples))
 
 
 # save or load data
 #np.save('emnist_dataset.npy', emnist_dataset)
 #emnist_dataset = np.load('emnist_dataset.npy',allow_pickle=True)
-
